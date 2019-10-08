36712ff1ba539e59cd7870167343f7979d3560df
Rania S <rsmmg@icloud.com>
2019-10-08 12:29:49 -0600

    Added temperature conversion

---
+++ TemperatureConversion/Defaultform.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Defaultform.aspx.cs" Inherits="TemperatureConversion.Defaultform" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Temperature Conversion</title>
+    <link href="Style.css" rel="stylesheet" />
+</head>
+
+<body>
+    <h1>Temperature Convertor: </h1>
+    <form id="form1" runat="server">
+        <div>
+            <table >
+                <tr>
+                    <td>
+                        <asp:Image ID="Image1" runat="server"  ImageUrl="~/images/thermometer.jpg" />
+                    </td>
+                    <td class="column1">
+                       
+            <asp:Label ID="lblFrom" runat="server" cssclass="labelfrom" Text="From: "></asp:Label>
+
+            <asp:Label ID="lblTo0" runat="server" cssclass="labelto" Text="To:"></asp:Label><br/>
+            <asp:DropDownList ID="ddlFrom" runat="server">
+                <asp:ListItem>Celsius</asp:ListItem>
+                <asp:ListItem>Fahrenheit</asp:ListItem>
+                <asp:ListItem>Kelvin</asp:ListItem>
+            </asp:DropDownList>
+
+            <asp:DropDownList ID="ddlTo" runat="server">
+                <asp:ListItem>Fahrenheit</asp:ListItem>
+                <asp:ListItem>Celsius</asp:ListItem>
+                <asp:ListItem>Kelvin</asp:ListItem>
+            </asp:DropDownList><br/><br />
+                          
+
+
+            <asp:Label ID="LblInputTemp" runat="server"  Text="Input Temperature: "></asp:Label>
+
+            <asp:TextBox ID="txtInputTemp" runat="server" class="text"></asp:TextBox><br/><br/><br/>
+                        
+            <asp:Label ID="LblOutputTemp" runat="server"  Text="Output Temperature: "></asp:Label>
+
+            <asp:Label ID="lblresult" runat="server"></asp:Label><br/>
+            
+                        
+            <asp:Button ID="btnConvert" runat="server"   Text="Convert"  OnClick="btnConvert_Click" CssClass="buttonconvert"  />
+
+            <asp:Button ID="btnClear" runat="server" CssClass="buttonclear"  Text="Clear"  OnClick="btnClear_Click"
+                CausesValidation="False"  />
+                    </td>
+                    <td class="coulmn2">
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="txtInputTemp"
+                            ErrorMessage="Please enter a value!" CssClass="validator" Display="Dynamic"></asp:RequiredFieldValidator>
+                        <asp:RangeValidator ID="RangeValidator1" runat="server" ControlToValidate="txtInputTemp" 
+                            ErrorMessage="The value entered has to be a number between -1000 to 1000" CssClass="validator" MaximumValue="1000"
+                            MinimumValue="-1000" Type="Double" Display="Dynamic"></asp:RangeValidator> <br /><br /><br />
+                        
+                    </td>
+                    <td class ="column3">
+                    </td>
+                </tr>
+            </table>
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ TemperatureConversion/Defaultform.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+
+namespace TemperatureConversion
+{
+    public partial class Defaultform : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        }
+
+        protected void btnConvert_Click(object sender, EventArgs e)
+        {
+            double outputTemp; 
+            double inputTemp = double.Parse(txtInputTemp.Text);
+
+            // if the "from tempetature" = Celsius
+            if (ddlFrom.SelectedItem.Text == "Celsius")
+            {
+                // converting from Celsius to Fahrenheit
+                if (ddlTo.SelectedItem.Text == "Fahrenheit")
+                {
+                    
+                    outputTemp = (inputTemp * 9 / 5) + 32;
+                }
+
+                // converting from Celsius to Kelvin
+                else if (ddlTo.SelectedItem.Text == "Kelvin")
+                {
+                    outputTemp = inputTemp + 273.15;
+                }
+
+                // converting from Celsius to Celsius
+                else
+                {
+                    outputTemp = inputTemp;
+                }
+                lblresult.Text = outputTemp.ToString("F2");
+            }
+
+            // if the "from tempetature" = Fahrenheit
+            if (ddlFrom.SelectedItem.Text == "Fahrenheit")
+            {
+                // converting from Fahrenheit to Fahrenheit
+                if (ddlTo.SelectedItem.Text == "Fahrenheit")
+                {
+
+                    outputTemp = inputTemp ;
+                }
+                
+                // converting from Fahrenheit to Kelvin
+                else if (ddlTo.SelectedItem.Text == "Kelvin")
+                {
+                    outputTemp = (inputTemp + 459.67) * 5/9;
+                }
+                
+                // converting from Fahrenheit to Celsius
+                else
+                {
+                    outputTemp = (inputTemp - 32) * 5/9;
+                }
+                lblresult.Text = outputTemp.ToString("F2");
+            }
+
+            // if the "from tempetature" = Kelvin
+            if (ddlFrom.SelectedItem.Text == "Kelvin")
+            {
+                // converting from Kelvin to Fahrenheit
+                if (ddlTo.SelectedItem.Text == "Fahrenheit")
+                {
+                   
+                    outputTemp = inputTemp * 9/5 - 459.67;
+                }
+
+                // converting from Kelvin to Kelvin
+                else if (ddlTo.SelectedItem.Text == "Kelvin")
+                {
+                    outputTemp = inputTemp;
+                }
+
+                // converting from Kelvin to Celsius
+                else
+                {
+                    outputTemp = inputTemp - 273.15;
+                }
+                lblresult.Text = outputTemp.ToString("F2");
+            }
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            // Clear button  erases contents from the input text box and output lable
+            // and resets the conversion selection to the default one"Celsius to Fahrenheit"
+
+            txtInputTemp.Text = "";
+            lblresult.Text = "";
+            ddlFrom.SelectedIndex = 0;
+            ddlTo.SelectedIndex = 0;
+
+        }
+    }
+}
